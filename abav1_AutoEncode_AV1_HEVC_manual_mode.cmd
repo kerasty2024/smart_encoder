057482e3e24b7ae99ae0ab389d292eafafc5d937
Kerasty <kerasty20220803@protonmail.com>
2024-12-19 14:26:57 +0900

    updated cmds

--- abav1_AutoEncode_AV1_HEVC_manual_mode.cmd
+++ abav1_AutoEncode_AV1_HEVC_manual_mode.cmd
@@ -1,7 +1,5 @@
 set conda_cmd_path="C:\Tools\cmds\lunch_conda.cmd"
 call %conda_cmd_path%
 call activate Encode
-git pull
-pip install -r requirements.txt
 python -O "C:\Tools\Python\encode\main.py" --processes 3 --move-raw-file --manual-mode
 pause
--- encode_iPhone_XR_python.cmd
+++ encode_iPhone_XR_python.cmd
@@ -1,7 +1,5 @@
 set conda_cmd_path="C:\Tools\cmds\lunch_conda.cmd"
 call %conda_cmd_path%
 call activate Encode
-git pull
-pip install -r requirements.txt
 python -O "C:\Tools\Python\encode\iPhone_encode_main.py" --processes 1 --not-rename
 pause
