73a6b04cdd949d5587985db2d54bebacb043f14b
Kerasty <kerasty20220803@protonmail.com>
2024-12-19 12:03:46 +0900

    updated cmds

--- abav1_AutoEncode_AV1_HEVC_manual_mode.cmd
+++ abav1_AutoEncode_AV1_HEVC_manual_mode.cmd
@@ -1,4 +1,4 @@
 set root_cmd_path="C:\Tools\Python\encode\abav1_AutoEncode_AV1_root.cmd"
 call %root_cmd_path%
-python -O "C:\Tools\Python\encode\iPhone_encode_main.py" --processes 1 --not-rename
+python -O "C:\Tools\Python\encode\main.py" --processes 3 --move-raw-file --manual-mode
 pause
--- encode_iPhone_XR_python_audio_only.cmd
+++ encode_iPhone_XR_python_audio_only.cmd
@@ -1,5 +1,4 @@
-chcp 65001
-call "C:\Users\lapi\anaconda3\Scripts\activate.bat"
-call activate Encode
+set root_cmd_path="C:\Tools\Python\encode\abav1_AutoEncode_AV1_root.cmd"
+call %root_cmd_path%
 python -O "C:\Tools\Python\encode\iPhone_encode_main.py" --processes 5 --not-rename --audio-only --move-raw-file
 pause
