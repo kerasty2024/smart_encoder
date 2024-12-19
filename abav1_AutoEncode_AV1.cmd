7801c45ffa38d96a121f5e845dbd898c12fb3e5b
Kerasty <kerasty20220803@protonmail.com>
2024-12-19 15:02:59 +0900

    processors: 3 -> 2

--- abav1_AutoEncode_AV1.cmd
+++ abav1_AutoEncode_AV1.cmd
@@ -1,5 +1,5 @@
 set conda_cmd_path="C:\Tools\cmds\lunch_conda.cmd"
 call %conda_cmd_path%
 call activate Encode
-python -O "C:\Tools\Python\encode\main.py" --processes 3 --move-raw-file
+python -O "C:\Tools\Python\encode\main.py" --processes 2 --move-raw-file
 pause
