4f9f1229b07be8ba88ed0cce16dec7e4c052ac06
Kerasty <kerasty20220803@protonmail.com>
2025-01-07 14:56:54 +0900

    ProcessFiles: default set to be not rename dirs and files

--- abav1_AutoEncode_AV1.cmd
+++ abav1_AutoEncode_AV1.cmd
@@ -1,5 +1,5 @@
 set conda_cmd_path="C:\Tools\cmds\lunch_conda.cmd"
 call %conda_cmd_path%
 call activate Encode
-python -O "C:\Tools\Python\encode\main.py" --processes 2 --move-raw-file --not-raname
+python -O "C:\Tools\Python\encode\main.py" --processes 2 --move-raw-file --not-rename
 pause
